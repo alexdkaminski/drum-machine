185f617d122c2dac324608cbd166148e057a3b51
Alex Kaminski <alexdkaminski@gmail.com>
2019-07-26 17:50:00 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styled from 'styled-components/macro';
+import tw from 'tailwind.macro';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Header = styled.header`
+${tw`bg-black min-h-screen flex flex-col items-center justify-center text-xl text-white`};
+`;
+
+const sounds = [{name: ''}]
+
+class DrumMachine extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id='drum-machine'>
+        <div id='display'>
+        </div>
+        <DrumPad/>
     </div>
-  );
+    )
+  }
+}
+
+class DrumPad extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='drum-pad'>
+          <audio>
+            <source src={props.source} type='audio/wav'/>
+          </audio>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div css={tw`text-center`}>
+        <Header>
+        <p css={tw`text-blue-300`}>
+          Drum Machine
+        </p>
+        <DrumMachine />
+        </Header>
+      </div>
+    );
+  } 
 }
 
 export default App;
