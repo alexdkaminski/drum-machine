2c54452ea02b3d51791b1a5f212ceec67bda35ee
Alex Kaminski <alexdkaminski@gmail.com>
2019-07-30 07:09:25 +1000

    Add styling to grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import styled from 'styled-components/macro';
-import tw from 'tailwind.macro';
-import clap from './samples/clap-808.wav'
-import cowbell from './samples/cowbell-808.wav'
-import crash from './samples/crash-808.wav'
-import hihat from './samples/hihat-808.wav'
-import kick from './samples/kick-808.wav'
-import openhat from './samples/openhat-808.wav'
-import perc from './samples/perc-808.wav'
-import snare from './samples/snare-808.wav'
-import tom from './samples/tom-808.wav'
+import React, { Component } from "react";
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+import clap from "./samples/clap-808.wav";
+import cowbell from "./samples/cowbell-808.wav";
+import crash from "./samples/crash-808.wav";
+import hihat from "./samples/hihat-808.wav";
+import kick from "./samples/kick-808.wav";
+import openhat from "./samples/openhat-808.wav";
+import perc from "./samples/perc-808.wav";
+import snare from "./samples/snare-808.wav";
+import tom from "./samples/tom-808.wav";
+import { create } from "istanbul-reports";
 
-const Header = styled.header`
-${tw`bg-black min-h-screen flex flex-col items-center justify-center text-xl text-white`};
+const DrumPadGrid = styled.div`
+  ${tw`px-56 p-y-12 mx-auto`}
+`;
+const DrumPadRow = styled.div`
+  ${tw`flex mb-10 h-auto`}
+`;
+const DrumPadColumn = styled.div`
+  ${tw`w-1/3 bg-gray-400 h-auto mx-5`}
+`;
+const Button = styled.button`
+  ${tw`bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 font-sans border-none h-64 w-full text-3xl`}
 `;
 
 class DrumPad extends Component {
@@ -22,96 +32,76 @@ class DrumPad extends Component {
   }
 
   playAudio = () => {
-    this.playButton.current.play()
-  }
+    this.playButton.current.play();
+  };
 
   render() {
     return (
-      <div className='drum-pad'>
+      <DrumPadColumn>
           <audio ref={this.playButton}>
-            <source src={this.props.file} type='audio/wav'/>
+            <source src={this.props.file} type="audio/wav" />
           </audio>
-          <button onClick={this.playAudio}></button>
-      </div>
-    )
+          <Button onClick={this.playAudio}>{this.props.name}</Button>
+      </DrumPadColumn>
+    );
   }
 }
 
-
 class DrumMachine extends Component {
-
-  renderDrumPad(sound,i) {
-   return (
-     <DrumPad 
-       key={i}
-       file={sound}
-     />
-   );
- }
- 
- createDrumPad = () => {
-  let drumPadGrid = [];
-  let rowCount = 3;
-  let colCount = 3;
-  let count = 0;
-  const sounds = [
-    {name: 'Clap', file: clap},
-    {name: 'Cowbell', file: cowbell},
-    {name: 'Crash', file: crash},
-    {name: 'Hi-hat', file: hihat},
-    {name: 'Kick', file: kick},
-    {name: 'Open-hat', file: openhat},
-    {name: 'Perc', file: perc},
-    {name: 'Snare', file: snare},
-    {name: 'Tom', file: tom}
-  ];
-  
-  for(let i = 0; i < rowCount; i++) {
-    let drumPads = [];
-    for (let j = 0; j < colCount; j++){
-
-      drumPads.push(
-        this.renderDrumPad(sounds[count].file,count)
-      )
-      count = count + 1
-    }
-    drumPadGrid.push(
-      <div className="drum-pad-row" key={i}>
-        {drumPads}
-      </div>
-    )
+  renderDrumPad(sound, name, i) {
+    return <DrumPad key={i} file={sound} name={name}/>;
   }
-console.log(count)
 
-  return drumPadGrid
- }
+  createDrumPad = () => {
+    let drumPadGrid = [];
+    let rowCount = 3;
+    let colCount = 3;
+    let count = 0;
+    const sounds = [
+      { name: "Clap", file: clap },
+      { name: "Cowbell", file: cowbell },
+      { name: "Crash", file: crash },
+      { name: "Hi-hat", file: hihat },
+      { name: "Kick", file: kick },
+      { name: "Open-hat", file: openhat },
+      { name: "Perc", file: perc },
+      { name: "Snare", file: snare },
+      { name: "Tom", file: tom }
+    ];
 
- render() {
-   return (
-     <div id='drum-machine'>
-       <div id='display'>
-       </div>
-       <div>
-         {this.createDrumPad()}
-       </div>
-   </div>
-   )
- }
+    for (let i = 0; i < rowCount; i++) {
+      let drumPads = [];
+      for (let j = 0; j < colCount; j++) {
+        drumPads.push(this.renderDrumPad(sounds[count].file, sounds[count].name, count));
+        count = count + 1;
+      }
+      drumPadGrid.push(<DrumPadRow key={i}>{drumPads}</DrumPadRow>);
+    }
+
+    return drumPadGrid;
+  };
+
+  render() {
+    return (
+      <div id="drum-machine">
+        <div id="display" />
+        {this.createDrumPad()}
+      </div>
+    );
+  }
 }
 
 class App extends Component {
   render() {
     return (
-      <div css={tw`text-center`}>
-        <Header>
-        <p css={tw`text-blue-300`}>
-          Drum Machine
-        </p>
-        <DrumMachine />
-        </Header>
+      <div css={tw`text-center bg-gray-800 pb-8 text-xl`}>
+        <h1 css={tw`text-blue-300 m-0 py-4`}>Drum Machine</h1>
+        <DrumPadGrid>
+          <DrumMachine />
+        </DrumPadGrid>
       </div>
     );
-  } 
+  }
 }
 
 export default App;
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      }
+    }
   },
   variants: {},
   plugins: []
