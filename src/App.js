8cdf75ec5e99d6cfc0ce613d4c106b6f31cc35c2
Alex Kaminski <alexdkaminski@gmail.com>
2019-07-30 05:59:55 +1000

    Add grid layout

--- src/App.js
+++ src/App.js
@@ -15,21 +15,45 @@ const Header = styled.header`
 ${tw`bg-black min-h-screen flex flex-col items-center justify-center text-xl text-white`};
 `;
 
+class DrumPad extends Component {
+  constructor(props) {
+    super(props);
+    this.playButton = React.createRef();
+  }
+
+  playAudio = () => {
+    this.playButton.current.play()
+  }
+
+  render() {
+    return (
+      <div className='drum-pad'>
+          <audio ref={this.playButton}>
+            <source src={this.props.file} type='audio/wav'/>
+          </audio>
+          <button onClick={this.playAudio}></button>
+      </div>
+    )
+  }
+}
 
 
 class DrumMachine extends Component {
 
-   renderDrumPad(sound,i) {
-    return (
-      <DrumPad 
-        key={i}
-        file={sound}
-      />
-    );
-  }
-  
-  createDrumPad = () => {
-  let drumPad = [];
+  renderDrumPad(sound,i) {
+   return (
+     <DrumPad 
+       key={i}
+       file={sound}
+     />
+   );
+ }
+ 
+ createDrumPad = () => {
+  let drumPadGrid = [];
+  let rowCount = 3;
+  let colCount = 3;
+  let count = 0;
   const sounds = [
     {name: 'Clap', file: clap},
     {name: 'Cowbell', file: cowbell},
@@ -41,40 +65,38 @@ class DrumMachine extends Component {
     {name: 'Snare', file: snare},
     {name: 'Tom', file: tom}
   ];
-  sounds.forEach((sound,i) => {
-    drumPad.push(
-      this.renderDrumPad(sound.file,i)
-    )
-  })
-  return drumPad
-  }
+  
+  for(let i = 0; i < rowCount; i++) {
+    let drumPads = [];
+    for (let j = 0; j < colCount; j++){
 
-  render() {
-    return (
-      <div id='drum-machine'>
-        <div id='display'>
-        </div>
-        <div>
-          {this.createDrumPad()}
-        </div>
-    </div>
-    )
-  }
-}
-
-class DrumPad extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className='drum-pad'>
-          <audio controls>
-            <source src={this.props.file} type='audio/wav'/>
-          </audio>
+      drumPads.push(
+        this.renderDrumPad(sounds[count].file,count)
+      )
+      count = count + 1
+    }
+    drumPadGrid.push(
+      <div className="drum-pad-row" key={i}>
+        {drumPads}
       </div>
     )
   }
+console.log(count)
+
+  return drumPadGrid
+ }
+
+ render() {
+   return (
+     <div id='drum-machine'>
+       <div id='display'>
+       </div>
+       <div>
+         {this.createDrumPad()}
+       </div>
+   </div>
+   )
+ }
 }
 
 class App extends Component {
