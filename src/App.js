9914240d282461b3c62db80919f7de1fce744f2d
Alex Kaminski <alexdkaminski@gmail.com>
2019-07-31 07:29:10 +1000

    Add keydown event

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import tom from "./samples/tom-808.wav";
 import { create } from "istanbul-reports";
 
 const DrumPadGrid = styled.div`
-  ${tw`px-56 p-y-12 mx-auto`}
+  ${tw`px-56 p-y-12 mx-auto max-w-4xl`}
 `;
 const DrumPadRow = styled.div`
   ${tw`flex mb-10 h-auto`}
@@ -22,34 +22,50 @@ const DrumPadColumn = styled.div`
   ${tw`w-1/3 bg-gray-400 h-auto mx-5`}
 `;
 const Button = styled.button`
-  ${tw`bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 font-sans border-none h-64 w-full text-3xl`}
+  ${tw`bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 font-sans border-none h-48 w-full text-3xl`}
 `;
 
 class DrumPad extends Component {
   constructor(props) {
     super(props);
+    this.audio = React.createRef();
     this.playButton = React.createRef();
   }
 
-  playAudio = () => {
-    this.playButton.current.play();
+  componentDidMount() {
+    document.addEventListener('keydown',this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleClick = () => {
+    this.audio.current.play();
   };
+
+  handleKeyDown = (e) => {
+    if(e.key === this.props.keyLetter) {
+      // Play sound and depress button
+      this.audio.current.play();
+    }
+  }
 
   render() {
     return (
       <DrumPadColumn>
-          <audio ref={this.playButton}>
+          <audio ref={this.audio}>
             <source src={this.props.file} type="audio/wav" />
           </audio>
-          <Button onClick={this.playAudio}>{this.props.name}</Button>
+          <Button onClick={this.handleClick} ref={this.playButton} tabIndex="0">{this.props.name} </Button>
       </DrumPadColumn>
     );
   }
 }
 
 class DrumMachine extends Component {
-  renderDrumPad(sound, name, i) {
-    return <DrumPad key={i} file={sound} name={name}/>;
+  renderDrumPad(sound, name, keyLetter, i) {
+    return <DrumPad key={i} file={sound} name={name} keyLetter={keyLetter}/>;
   }
 
   createDrumPad = () => {
@@ -58,21 +74,21 @@ class DrumMachine extends Component {
     let colCount = 3;
     let count = 0;
     const sounds = [
-      { name: "Clap", file: clap },
-      { name: "Cowbell", file: cowbell },
-      { name: "Crash", file: crash },
-      { name: "Hi-hat", file: hihat },
-      { name: "Kick", file: kick },
-      { name: "Open-hat", file: openhat },
-      { name: "Perc", file: perc },
-      { name: "Snare", file: snare },
-      { name: "Tom", file: tom }
+      { name: "Clap", file: clap, keyLetter: "q" },
+      { name: "Cowbell", file: cowbell, keyLetter: "w" },
+      { name: "Crash", file: crash, keyLetter: "e" },
+      { name: "Hi-hat", file: hihat, keyLetter: "a" },
+      { name: "Kick", file: kick, keyLetter: "s" },
+      { name: "Open-hat", file: openhat, keyLetter: "d" },
+      { name: "Perc", file: perc, keyLetter: "z" },
+      { name: "Snare", file: snare, keyLetter: "x" },
+      { name: "Tom", file: tom, keyLetter: "c" }
     ];
 
     for (let i = 0; i < rowCount; i++) {
       let drumPads = [];
       for (let j = 0; j < colCount; j++) {
-        drumPads.push(this.renderDrumPad(sounds[count].file, sounds[count].name, count));
+        drumPads.push(this.renderDrumPad(sounds[count].file, sounds[count].name, sounds[count].keyLetter, count));
         count = count + 1;
       }
       drumPadGrid.push(<DrumPadRow key={i}>{drumPads}</DrumPadRow>);
