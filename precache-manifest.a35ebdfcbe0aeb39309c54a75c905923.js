9009e16117bb3534407f125e13767dcbaf111219
Alex Kaminski <alexdkaminski@gmail.com>
2019-08-03 11:26:25 +1000

    Updates

---
+++ precache-manifest.a35ebdfcbe0aeb39309c54a75c905923.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec2d99c8d9481a17e568cdf177cafe0",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "c162aec806820e92bf17",
+    "url": "/drum-machine/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "fbea0a64591f788351f5",
+    "url": "/drum-machine/static/js/2.5f557c3e.chunk.js"
+  },
+  {
+    "revision": "c162aec806820e92bf17",
+    "url": "/drum-machine/static/js/main.a79e8f95.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "39f9581affa38e7f9322157d85e1ff57",
+    "url": "/drum-machine/static/media/clap-808.39f9581a.wav"
+  },
+  {
+    "revision": "470b5edb9f8d1c23184513a3d5829967",
+    "url": "/drum-machine/static/media/cowbell-808.470b5edb.wav"
+  },
+  {
+    "revision": "de6f53e55594a772088b64fbefcae134",
+    "url": "/drum-machine/static/media/crash-808.de6f53e5.wav"
+  },
+  {
+    "revision": "46ba2a481c9caaaf9c8acfd65e4626e5",
+    "url": "/drum-machine/static/media/hihat-808.46ba2a48.wav"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drum-machine/static/media/kick-808.550be808.wav"
+  },
+  {
+    "revision": "056a627a4d91e0bd881ab36ca89ed823",
+    "url": "/drum-machine/static/media/openhat-808.056a627a.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drum-machine/static/media/perc-808.2a6bb6f5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drum-machine/static/media/snare-808.80c4129b.wav"
+  },
+  {
+    "revision": "446e78dd8d40065dfc499b4468610296",
+    "url": "/drum-machine/static/media/tom-808.446e78dd.wav"
+  }
+]);
